4bc8ef095508c45382c72ed341822ac5479bf9b3
Osama Al-Chalabi <alchala@uwindsor.ca>
2020-11-12 23:38:30 -0500

    created base layout

---
+++ db_conn.php
@@ -0,0 +1,19 @@
+<?php
+
+
+$sName = "localhost";
+$uName = "root";
+$pass = "";
+$db_name = "to_do_list";
+
+
+
+try {
+
+    $conn = new PDO("mysql:host=$sName;db_name=$db_name", $uName, $pass);
+    echo "Connection Successful :D ";
+
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    echo "Connection Failed : " . $e->getMessage();
+}
---
+++ index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>To-Do List</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+
+    <!-- This is the entire project section -->
+    <div class="main-section">
+        <!-- This is the add section  -->
+        <div class="add-section">
+            <form action="">
+
+                <input type="text" name="title" placeholder="This field is required">
+                <button type="submit">Add &nbsp;<span>&#43;</span></button>
+            </form>
+        </div>
+        <!-- End of Add Section -->
+
+        <!-- Start of the show-to-do list section  -->
+        <div class="show-todo-section">
+
+            <div class="todo-item">
+                <input type="checkbox">
+                <h2>Lorem, ipsum dolor.</h2>
+                <br>
+                <small>Created: 3/2/2022</small>
+            </div>
+
+
+
+        </div>
+
+
+
+
+    </div>
+    <!-- End of the project section -->
+</body>
+
+</html>
